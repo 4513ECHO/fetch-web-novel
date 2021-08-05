b47cb0329907b2419ab7f713fb13d33673d038a6
4513ECHO <4513echo@gmail.com>
2021-08-05 13:55:22 +0000

    [update] いったん完成形。0.1.0として公開

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # fetch-web-novel
+
+小説投稿サイトから指定した小説の内容を取得するCLIツール
+
+> **WARNING**
+> このツールの使用は個人での利用にとどめ、絶対に日本の法律に違反しないようにしてください。また、Webスクレイピングに関する注意事項([ここ](https://qiita.com/nezuq/items/c5e827e1827e7cb29011)などが詳しいです)を必ず守ってください。
+
+## Usage
+
+### 準備
+
+依存パッケージが`requestments.txt`に書いてあります。お使いのパッケージマネージャーでインストールしてください。
+
+```pipの場合
+pip install -r requestments.txt
+```
+
+### コマンド
+
+作者の信条によりヘルプは全て英語です。わからないときはこのREADMEを読むか、issueを立ててください。
+
+```
+usage: ./fetch_web_novel.py [options...] <novel_code>
+
+CLI tool to retrieve the contents of a specified novel from a novel submission website
+
+positional arguments:
+  novel_code    set the code of the novel to be retrieve
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -N, --narou   set the destination website to 'Shosetsuka ni Narou'
+  -H, --hameln  set the destination website to 'Hameln'
+```
+
+| オプション   |        |
+|:------------:|--------|
+| -h, --help   | ヘルプを表示します |
+| <novel_code> | 取得したい小説のコードを設定します |
+| -N, --narou  | 取得先のWebサイトを「小説家になろう」に設定します |
+| -H, --hameln | 取得先のWebサイトを「小説家になろう」に設定します |
+
+## License
+
+MIT License
+
---
+++ fetch_web_novel.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser, Namespace
+import enum
+import time
+from typing import Union, Optional, Callable
+import os
+
+from bs4 import BeautifulSoup, element
+import requests
+
+
+class Website(enum.Enum):
+    narou = enum.auto()
+    hameln = enum.auto()
+
+
+headers = {
+    "User-agent": "fetch-web-novel Crawler/0.1.0 (https://github.com/4513ECHO/fetch-web-novel)",
+    "Crawl-delay": "3",
+}
+site_data = {
+    Website.narou: {
+        "name": "narou",
+        "url": "https://ncode.syosetu.com/{0}/{1}/",
+        "headers": headers,
+        "honbun": "#novel_honbun",
+        "backnumber": "#novel_no",
+    },
+    Website.hameln: {
+        "name": "hameln",
+        "url": "https://syosetu.org/novel/{0}/{1}.html",
+        "headers": headers,
+        "honbun": "#honbun",
+        "backnumber": ".ss > div:first-of-type",
+    },
+}
+
+
+class Novel:
+    def __init__(self, website: Website, novel_code: str) -> None:
+        self.website: Website = website
+        self.site_data: dict[str] = site_data[self.website]
+        self.novel_code: str = novel_code
+
+    def _request(self, num: int) -> BeautifulSoup:
+        res: requests.medels.Response = requests.get(
+            self.site_data["url"].format(self.novel_code, num),
+            headers=self.site_data["headers"],
+        )
+        return BeautifulSoup(res.text, "html.parser")
+
+    def get_backnumber(self) -> int:
+        soup: BeautifulSoup = self._request(1)
+        backnumber: element.Tag = soup.select_one(self.site_data["backnumber"]).text
+        return int(backnumber.split("/")[-1])
+
+    def get_honbun(self, num: int) -> str:
+        soup: BeautifulSoup = self._request(num)
+        honbun: element.Tag = soup.select_one(self.site_data["honbun"])
+        if self.site_data["name"] == "narou":
+            return self._narou(honbun)
+        if self.site_data["name"] == "hameln":
+            return self._hameln(honbun)
+
+    def _narou(self, soup: element.Tag) -> str:
+        return soup.text
+
+    def _hameln(self, soup: element.Tag) -> str:
+        text = map(lambda x: x.text, soup)
+        return "\n".join(text)
+
+
+# def to_SJIS(self, text: str) -> str:
+#     return text
+
+
+def write_file(
+    name: Union[str, int], text: str, directory: Optional[str] = None
+) -> None:
+    if directory is None:
+        with open(f"{name}.txt", "w") as f:
+            f.write(text)
+            return
+    os.makedirs(os.path.expanduser(directory), exist_ok=True)
+    with open(f"{directory}/{name}.txt", "w") as f:
+        f.write(text)
+
+
+# def show_progress(max: int, current: int) -> None:
+#     pass
+
+
+def get_args() -> Namespace:
+    parser = ArgumentParser(
+        description="CLI tool to retrieve the contents of "\
+                    "a specified novel from a novel submission website",
+        usage="./%(prog)s [options...] <novel_code>"
+    )
+    site = parser.add_mutually_exclusive_group(required=True)
+    site.add_argument(
+        "-N", "--narou",
+        action="store_true",
+        help="set the destination website to 'Shosetsuka ni Narou'",
+    )
+    site.add_argument(
+        "-H", "--hameln",
+        action="store_true",
+        help="set the destination website to 'Hameln'",
+    )
+    parser.add_argument("novel_code", help="set the code of the novel to be retrieve")
+    # parser.add_argument(
+    #     "-b", "--back-number",
+    #     default=None,
+    #     metavar="<INT>-<INT>"
+    #     type=lambda x: map(int, x.split("-"))[:2],
+    # )
+    # parser.add_argument("-d", "--dirname", default=None)
+    # parser.add_argument("-J", "--toSJIS", action="store_true")
+    args = parser.parse_args()
+    return args
+
+
+def main() -> None:
+    args: Namespace = get_args()
+    if args.narou:
+        website = Website.narou
+    elif args.hameln:
+        website = Website.hameln
+    novel: Novel = Novel(website, args.novel_code)
+    directory: str = novel.novel_code
+    max_num: int = novel.get_backnumber()
+    for x in range(1, max_num + 1):
+        honbun: str = novel.get_honbun(x)
+        write_file(x, honbun, directory)
+        time.sleep(1)
+        print(f"\rWriting... ({x} / {max_num})", end="")
+    print("\nDone.")
+
+
+def return_status(func: Callable) -> int:
+    try:
+        func()
+        return 0
+    except Exception as err:
+        print(f"[ERROR]:{err}")
+        return 1
+
+
+if __name__ == "__main__":
+    return_status(main)
